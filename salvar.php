d8254a7b3077031f0f809798330e022534573a6f
Arthur-Rib <arthurbrito2003@gmail.com>
2021-10-05 19:50:39 +0000

    ajustes

--- salvar.php
+++ salvar.php
@@ -3,8 +3,8 @@
 	$nome = $_POST['nome'];
 	$email = $_POST['email'];
 	$senha = $_POST['senha'];
-	$sql = "INSERT INTO usuario (NOME, EMAIL, SENHA) VALUES ('$nome', '$email', '$senha')";
-	mysqli_query($connect, $sql) or die(error());
+	$sql = "INSERT INTO USUARIO (NOME, EMAIL, SENHA) VALUES ('$nome', '$email', '$senha')";
+	mysqli_query($connect, $sql) or die("deu erro mano");
 	$response = array("success" => true);
 	echo json_encode($response);
 ?> 
